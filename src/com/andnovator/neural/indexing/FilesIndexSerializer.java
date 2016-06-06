ac6ab6f012cc387ef35175d551ecfd7c6cccb029
Ivan Novikov <nia.afti@gmail.com>
2016-06-06 22:29:19 +0300

    Add UML diagrams

--- src/com/andnovator/neural/indexing/FilesIndexSerializer.java
+++ src/com/andnovator/neural/indexing/FilesIndexSerializer.java
@@ -4,4 +4,13 @@ package com.andnovator.neural.indexing;
  * Created by novator on 06.06.2016.
  */
 public class FilesIndexSerializer {
+
+    public void serialize(FilesIndex index) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    public FilesIndex deserialize() {
+//        throw new UnsupportedOperationException("not yet implemented");
+        return new FilesIndex();
+    }
 }
