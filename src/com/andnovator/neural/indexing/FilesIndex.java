7182888a69e079334957ea53080a4cd06533eaef
Ivan Novikov <novikov@pragmatix-corp.com>
2016-06-07 17:57:36 +0300

    Add storing all keySets in one wordsByFile

--- src/com/andnovator/neural/indexing/FilesIndex.java
+++ src/com/andnovator/neural/indexing/FilesIndex.java
@@ -9,6 +9,9 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by novator on 16.05.2016.
@@ -19,6 +22,7 @@ public class FilesIndex {
     List<OneFileNeuralIndex> fileIndexNILst = new ArrayList<>();
     Set<String> allFileWords = new HashSet<>();
     Set<String> allWords;
+    private List<Set<String>> wordsByFile;
 
     private int wordMaxLength = 20;
     static private final int bitsForChar = 8;
@@ -125,26 +129,52 @@ public class FilesIndex {
             filesPath = Files.walk(dirPath)
                     .filter(p -> plainTxtExt
                             .contains(p.toString().toLowerCase().substring(p.toString().lastIndexOf('.'))))
-                    .collect(Collectors.toList());
+                    .collect(toList());
         } /*else {
             throw exception...
         }*/
+
         Map<String, PosFreqPair> wordsMapOneFile;
-        OneFileNeuralIndex oneFileNI;
+        OneFileNeuralIndex oneFileNI = new OneFileNeuralIndex();
+
         List<String > allWordsLst = new ArrayList<>();
+        wordsByFile = new ArrayList<>(filesPath.size());
 
         for (Path path : filesPath) {
             wordsMapOneFile = IndexingFileLoader.loadFile(path.toString());
             allFileWords.addAll(wordsMapOneFile.keySet());
             allWords.addAll(wordsMapOneFile.keySet());
-            if ((ThreadLocalRandom.current().nextInt(10)>7) || (allWordsLst.isEmpty())) {
-                allWordsLst.clear();
-                allWordsLst.addAll(allWords);
-            }
+            wordsByFile.add(wordsMapOneFile.keySet());
+//            if ((ThreadLocalRandom.current().nextInt(10)>7) || (allWordsLst.isEmpty())) {
+//                allWordsLst.clear();
+//                allWordsLst.addAll(allWords);
+//            }
             oneFileNI = new OneFileNeuralIndex();
             oneFileNI.setNetworkMinMSE(0.01);
             oneFileNI.trainIndex(wordsMapOneFile, allWordsLst);
         }
+
+        // Now train big net
+        List<List<Double>> wordsToFeed = new ArrayList<>(allFileWords.size());
+        List<List<Double>> trainingSample = new ArrayList<>(wordsToFeed.size());
+
+        for (String word : allFileWords) {
+            wordsToFeed.add(oneFileNI.strToDoubleBits(word));
+            trainingSample.add(findFilesForWord(word));
+
+        }
+
+
+        NeuralNetwork allFilesNet = new NeuralNetwork(0, 0, 0, 0); // TODO: correct values
+        allFilesNet.Train(wordsToFeed, trainingSample);
+    }
+
+    private List<Double> findFilesForWord(String word) {
+        return IntStream.range(0, filesPath.size())
+                        // если в файле #fileIndex содержится слово word - то +1, иначе -1
+                        .map( fileIndex -> wordsByFile.get(fileIndex).contains(word) ? +1 : -1)
+                        .mapToObj(Double::valueOf)
+                        .collect(toList());
     }
 
     public void addFileNI(OneFileNeuralIndex niNN) {
