8ada1d2c666a8d670da236513f2bd110a77e040e
Andrey <andnovator@gmail.com>
2016-06-10 12:55:04 +0300

    Some test for FilesIndex

--- test/com/andnovator/neural/indexing/FilesIndexTest.java
+++ test/com/andnovator/neural/indexing/FilesIndexTest.java
@@ -10,15 +10,39 @@ import java.util.Map;
  * Created by novator on 09.06.2016.
  */
 public class FilesIndexTest {
+    String dirPath = "C:\\Users\\novator\\Desktop\\ind";
 
     @Test
     public void filesIndexTrainTest() throws Exception {
-        String dirPath = "C:\\Users\\novator\\Desktop\\ind";
         FilesIndex fni = new FilesIndex();
         fni.setNetworkMinMSE(0.1);
         fni.indexDir(dirPath);
         String word = "is";
         Map<Path, PosFreqPair> filesWordPosMap = fni.wordSearch(word);
+        if (filesWordPosMap.isEmpty()) {
+            System.out.println("Word '" + word + "' not found in dir: " + dirPath);
+        } else {
+            System.out.println("Word '" + word + "' was found in files:");
+            filesWordPosMap.forEach((k, v) -> System.out.println(k + ": Pos - " + v.getPos()+", freq - "+v.getFreq()));
+        }
+        new FilesIndexSerializer(dirPath+"\\index.ser").serialize(fni);
+    }
+
+    @Test
+    public void bTest() throws Exception {
+        FilesIndex fni = new FilesIndex();
+        fni = new FilesIndexSerializer(dirPath+"\\index.ser").deserialize();
+        fni.setNetworkMinMSE(0.01);
+        String word = "is";
+        Map<Path, PosFreqPair> filesWordPosMap = fni.wordSearch(word);
+        if (filesWordPosMap.isEmpty()) {
+            System.out.println("Word '" + word + "' not found in dir: " + dirPath);
+        } else {
+            System.out.println("Word '" + word + "' was found in files:");
+            filesWordPosMap.forEach((k, v) -> System.out.println(k + ": Pos - " + v.getPos()+", freq - "+v.getFreq()));
+        }
+        word = "qwerty";
+        filesWordPosMap = fni.wordSearch(word);
         if (filesWordPosMap.isEmpty()) {
             System.out.println("Word '" + word + "' not found in dir: " + dirPath);
         } else {
