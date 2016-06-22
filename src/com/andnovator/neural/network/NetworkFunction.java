c287a8e3f013bb14e6f3430cfa42e74525593316
Andrey <andnovator@gmail.com>
2016-06-22 21:48:49 +0300

    Add Copyright, GPLv3
    Add Copyright headers with GPLv3 header

--- src/com/andnovator/neural/network/NetworkFunction.java
+++ src/com/andnovator/neural/network/NetworkFunction.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright © 2016 Andrey Novikov.
+ *
+ * Java code is based on Sovietmade (https://github.com/Sovietmade) c++
+ * code (https://github.com/Sovietmade/NeuralNetworks)
+ *
+ * This file is part of neural_index.
+ *
+ * neural_index is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * neural_index is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with neural_index.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.andnovator.neural.network;
 
 /**
